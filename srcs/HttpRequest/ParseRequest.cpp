b278a0b51562ea56731d0986b1d55efa6467a076
Касим Байбиков <kasim.baybikov@gmail.com>
2022-04-10 09:58:40 +0300

    найден и исправлен баг, при котором getline не читал буффер и строка оставалось неизменной

--- srcs/HttpRequest/ParseRequest.cpp
+++ srcs/HttpRequest/ParseRequest.cpp
@@ -3,11 +3,15 @@
 #include "webserv.hpp"
 #include <sstream>
 #include <sys/socket.h>
+#define BUFSIZE 5
 
 void HtppRequest::ParseRequest(int &close_connect, int fd) {
-	char buffer[1000];
-	memset(buffer, 0, 1000);
+	char buffer[BUFSIZE];
+	memset(buffer, 0, BUFSIZE);
 	close_connect = 0;
+	std::istringstream input;
+	std::string tmp;
+	std::string line;
 	while (1)
 	{
 		int rc = recv(fd, buffer, sizeof(buffer), 0);
@@ -18,29 +22,41 @@ void HtppRequest::ParseRequest(int &close_connect, int fd) {
 			close_connect = 1;
 			break;
 		}
-		std::string line;
-		std::istringstream input(buffer);
+		std::string read_line(buffer, BUFSIZE);
+		if (tmp != "") {
+			line = tmp + read_line;
+		} else {
+			line = read_line;
+		}
+		std::istringstream input(line);
 		std::getline(input, line);
 			//std::cout << line << std::endl;
+		std::string pred_line;
 		while (1) {
-			std::cout << line << std::endl;
 			if (line == "\r") {
+				//конец запроса
+				std::cout << RED "END REQUEST" WHITE << std::endl;
 				break;
 			}
+			if (line == pred_line) { // в случае если getline не продолжает читать и изменять line
+				//std::cout << RED "This line is readed!" WHITE << std::endl;
+				//std::cout << YELLOW + line + WHITE << std::endl;
+				tmp = line;
+				break;
+			}
+			if (line.find('\r') != std::string::npos) {
+				//отмечаем каждую строчку
+				//std::cout << "find " << line << std::endl;
+				//тут необходимо парсить строки
+			} else {
+				tmp = line;
+				break;
+			}
+			pred_line = line;
 			std::getline(input, line);
+			//std::cout << "GELINE" << std::endl;
 		}
-		std::cout << RED "END" WHITE << std::endl;
-		//std::cout << buffer << RED << "AAA" << WHITE;
-		//std::string RequestPart = buffer;
-		//while (line.find('\n') != std::string::npos) {
-			//std::cout << "AAA" << std::endl;
-			//memset(buffer, 0, 10000);
-			//if (RequestPart.find('\n') != std::string::npos) {
-				//line = RequestPart.substr(0, RequestPart.find('\n'));
-			//}
-			//else {
-				//end = RequestPart;
-			//}
+		memset(buffer, 0, BUFSIZE);
 			//if (this->Method == "") {
 				//this->Method = line.substr(0, line.find(' '));
 				//line = line.substr(line.find(' ') + 1);
@@ -48,9 +64,6 @@ void HtppRequest::ParseRequest(int &close_connect, int fd) {
 				//line = line.substr(line.find(' ') + 1);
 				//this->Version = line.substr(0, line.find(' '));
 			//}
-		//}
-		//Server::Log(this->Method + " request with Path " + this->Path + "from " + std::to_string(fd) + " fd");
-		//std::cout << this->Method << std::endl << this->Path << std::endl << this->Version << std::endl;
 		//нужно попробовать getline
 	}
 }
