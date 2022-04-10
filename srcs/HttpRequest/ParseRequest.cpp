6b1b01eaa65f508b96b7417dccb3cf07be85cbab
Касим Байбиков <kasim.baybikov@gmail.com>
2022-04-10 10:21:30 +0300

    start new parsing

--- srcs/HttpRequest/ParseRequest.cpp
+++ srcs/HttpRequest/ParseRequest.cpp
@@ -24,39 +24,9 @@ void HtppRequest::ParseRequest(int &close_connect, int fd) {
 			close_connect = 1;
 			break;
 		}
-		std::string read_line(buffer, BUFSIZE);
-		if (tmp != "") {
-			line = tmp + read_line;
-			tmp = "";
-		} else {
-			line = read_line;
-		}
-		std::istringstream input(line);
-		//std::getline(input, line);
-		//while (1) {
-			if (line == "\r") {
-				//конец запроса
-				std::cout << RED "END REQUEST" WHITE << std::endl;
-				break;
-			}
-			if (line == pred_line) { // в случае если getline не продолжает читать и изменять line
-				//std::cout << RED "This line is readed!" WHITE << std::endl;
-				//std::cout << YELLOW + line + WHITE << std::endl;
-				tmp = line;
-				break;
-			}
-			if (line.find('\r') != std::string::npos) {
-				//отмечаем каждую строчку
-				std::cout << line << std::endl;
-				//тут необходимо парсить строки
-			} else {
-				tmp = line;
-				break;
-			}
-			pred_line = line;
-			//std::getline(input, line);
-			//std::cout << "GELINE" << std::endl;
-		//}
-		memset(buffer, 0, BUFSIZE);
+
+		//создаю стрим, что читать из него getline'ом
+		std::string input_str(buffer, sizeof(buffer));
+		std::istringstream input(input_str);
 	}
 }
